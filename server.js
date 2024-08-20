52c54295c30613781eed2ba17c6e74c095839d34
ccie7599 <bapley@akamai.com>
2024-08-20 10:28:46 -0400

    Update server.js

--- server.js
+++ server.js
@@ -11,13 +11,35 @@ const privateKey = fs.readFileSync('/etc/privkey.pem', 'utf8');
 const certificate = fs.readFileSync('/etc/fullchain.pem', 'utf8');
 const credentials = { key: privateKey, cert: certificate };
 
-// Define the route
+// Define the /get route
 app.get('/get', (req, res) => {
   const topic = req.query.topic;
 
   if (!topic) {
-    return res.status(400).send('Bad Request: topic query parameter is required'
-);
+    return res.status(400).send('Bad Request: topic query parameter is required');
+  }
+
+  const filePath = path.join('/tmp/db', `${topic}`);
+
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        return res.status(404).send('Not Found: topic does not exist');
+      } else {
+        return res.status(500).send('Internal Server Error');
+      }
+    }
+
+    res.send(data);
+  });
+});
+
+// Define the /getlegacy route (duplicate of /get)
+app.get('/getlegacy', (req, res) => {
+  const topic = req.query.topic;
+
+  if (!topic) {
+    return res.status(400).send('Bad Request: topic query parameter is required');
   }
 
   const filePath = path.join('/tmp/db', `${topic}`);
